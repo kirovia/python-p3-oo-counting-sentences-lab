ffac22bf879dab7a8290e5065f582681166e41a9
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2025-06-03 12:49:47 -0400

    Passes all tests

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
 
+import re
+
 class MyString:
-  pass
+    def __init__(self, value=''):
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+    
+    @value.setter
+    def value(self, value):
+        if type(value) not in [str]:
+            print('The value must be a string.')
+        else:
+            self._value = value
+
+    def is_sentence(self):
+        return True if self.value[-1] == '.' else False
+    
+    def is_question(self):
+        return True if self.value[-1] == '?' else False
+    
+    def is_exclamation(self):
+        return True if self.value[-1] == '!' else False
+    
+    def count_sentences(self):
+        return len(re.split(r'[\.!?]+', self.value)) -1
